c81fc1ff1c0eaf59c65b0c4590d41668fa281398
Attila2710 <aronubrendan1@gmail.com>
2022-09-08 15:58:50 -0700

    my c script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,8 @@
  * Return: Alwaya 0 after printing the function
  */
 int main(void)
-{
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
+{	
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0)
 }
