76ad8d74818832d9f4cd97d61061818728e379a9
Attila2710 <aronubrendan1@gmail.com>
2022-09-08 14:50:34 -0700

    my c script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Alwaya 0 (success)
+ * Return: Alwaya 0 after printing the function
  */
 int main(void)
 {
- 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 } 
