e8ec8c8d634e050c3b73d9ee198641d1a5a7f1fb
Attila2710 <aronubrendan1@gmail.com>
2022-09-09 13:26:31 -0700

    new c script

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,30 +1,26 @@
-#include <studlib.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
-/**
- * main - Alphabetical characters printing
- * Return: Always (success)
- */
 int main(void)
 {
 	int n;
+
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
 
-	return (0)
+	printf("%d is positive\n", n);
+
+	else if (n < 0)
+
+	printf("%d is negative\n", n);
+
+	else
+
+	printf("%d is zero\n", n);
+
+	return (0);
 }
