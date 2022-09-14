83989c7bedd24991c111a35845791e3180a19fec
Attila2710 <aronubrendan1@gmail.com>
2022-09-14 11:59:16 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * main - print putchar
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
