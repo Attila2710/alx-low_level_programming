764b57c4d63114b6d8d42d6257b601b233139fa6
Attila2710 <aronubrendan1@gmail.com>
2022-09-16 17:50:40 -0700

    more nested loop

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,22 +15,22 @@ int main(void)
 	{
 		if (((i % 3) == 0) && ((i % 5) != 0))
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if ((i % 5) == 0 && ((i % 3) != 0))
 		{
 			if (i == 100)
 				printf("Buzz");
 			else
-				printf("Buzz ");
+				printf("Buzz");
 		}
 		else if ((i % 5) == 0 && ((i % 3) == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
 	}
 	printf("\n");
