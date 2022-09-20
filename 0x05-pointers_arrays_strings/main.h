573454f4c04a7523df295281bf8fe4cb75427950
Attila2710 <aronubrendan1@gmail.com>
2022-09-20 09:33:35 -0700

    pointers and arrays

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -18,7 +18,7 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *str);
-int _putchar(char *s);
+int _putchar(char c);
 int _atoi(char *s);
 
 #endif
