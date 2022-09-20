b003c988dd87703700ead20b7f1ce25e2aa067ce
Attila2710 <aronubrendan1@gmail.com>
2022-09-20 03:27:16 -0700

    pointers and arrays

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,12 +7,10 @@
  */
 int _strlen(char *s)
 {
-	int c = 0;
+	int len;
 
-	for (; *s != '\0'; s++)
-	{
-		c++;
-	}
-
-	return (c);
-
+	len = 0;
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
